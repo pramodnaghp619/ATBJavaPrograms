9ba07f9dc41ca63fca3a9b9aec5be75beca7de6e
Pramod nag <pramodnaghp619@gmail.com>
2024-09-15 08:51:20 +0530

    Add the files

---
+++ src/Sept13th/task2cal.java
@@ -0,0 +1,13 @@
+package Sept13th;
+
+public class task2cal {
+    public static void main(String[] args) {
+        double a = 34;
+        double b = 10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
